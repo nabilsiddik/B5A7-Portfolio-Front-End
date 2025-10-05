25e95e4bf791c6b18bd53e3d23eca60f972f2e52
nabilsiddik <nabilsiddik90@gmail.com>
2025-10-05 10:48:03 +0600

    social link added

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -3,25 +3,31 @@ import {
   RiGithubFill,
   RiGoogleFill,
   RiTwitterXFill,
-} from "@remixicon/react"
+} from "@remixicon/react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function SocialIcons() {
   return (
     <div className="inline-flex flex-wrap gap-2">
-      <Button variant="outline" aria-label="Login with Google" size="icon">
-        <RiGoogleFill size={16} aria-hidden="true" />
-      </Button>
-      <Button variant="outline" aria-label="Login with Facebook" size="icon">
-        <RiFacebookFill size={16} aria-hidden="true" />
-      </Button>
-      <Button variant="outline" aria-label="Login with X" size="icon">
-        <RiTwitterXFill size={16} aria-hidden="true" />
-      </Button>
-      <Button variant="outline" aria-label="Login with GitHub" size="icon">
-        <RiGithubFill size={16} aria-hidden="true" />
-      </Button>
+      <Link href="https://www.facebook.com/codewithnabil">
+        <Button variant="outline" aria-label="Login with Facebook" size="icon">
+          <RiFacebookFill size={16} aria-hidden="true" />
+        </Button>
+      </Link>
+
+      <Link href="https://x.com/SiddikNabil">
+        <Button variant="outline" aria-label="Login with X" size="icon">
+          <RiTwitterXFill size={16} aria-hidden="true" />
+        </Button>
+      </Link>
+
+      <Link href="https://github.com/nabilsiddik">
+        <Button variant="outline" aria-label="Login with GitHub" size="icon">
+          <RiGithubFill size={16} aria-hidden="true" />
+        </Button>
+      </Link>
     </div>
-  )
+  );
 }
